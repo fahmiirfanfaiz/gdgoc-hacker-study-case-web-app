0c944f5d82539a7165bbbefbab62c0650b15a4c2
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2025-11-28 11:02:07 +0700

    fix(shop): fix breadcrumb component layout

--- src/components/ui/Breadcrumb.jsx
+++ src/components/ui/Breadcrumb.jsx
@@ -23,7 +23,7 @@ const Breadcrumbs = () => {
 
   return (
     <nav className="w-full bg-white py-6">
-      <div className="container mx-auto px-4 sm:px-8 md:px-16 lg:ml-[16vw]">
+      <div className="container mx-auto px-4 sm:px-8 md:px-16 lg:ml-[13vw]">
         <ol className="flex items-center justify-center sm:justify-start gap-2 text-sm font-inter">
           {breadcrumbs.map((breadcrumb, index) => (
             <li key={breadcrumb.href} className="flex items-center gap-2">
